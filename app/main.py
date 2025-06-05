7658ad530ab8560ab1b7a0bd2be17a3cc1bd5952
JorbatyJuriy <136102634+JorbatyJuriy@users.noreply.github.com>
2025-06-05 14:22:20 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -31,7 +31,7 @@ app = FastAPI(title="ChatFlat API", lifespan=lifespan)
 # Налаштування CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","https://chatflat-frontend.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
